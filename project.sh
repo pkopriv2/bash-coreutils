53150fe15fd4b271f258a7bdf5cc337ca73399d8
Preston Koprivica <pkopriv2@gmail.com>
2013-08-19 01:10:18 -0500

    Adding remote repos

--- project.sh
+++ project.sh
@@ -3,3 +3,6 @@ version     "1.1.1"
 author      "Preston Koprivica"
 email       "pkopriv2@gmail.com"
 description "The standard bash library."
+
+snapshot_repo  "http://github.com/pkopriv2/bashum-snapshot.git"
+release_repo   "http://github.com/pkopriv2/bashum-main.git"
